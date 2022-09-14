005177fc995dbe8053b3fa79358a92cbca4773e4
Dimas Surya Dilana <dimassuryadilana@gmail.com>
2022-09-14 16:28:48 +0700

    layout fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      ungu: "#4A6FFF",
+      white: "#ffffff",
+      abu: "#95A2A9",
+      abuTua: "#9F9F9F",
+      abuMuda: "#D3D3D3",
+      black: "#2F2F2F",
+    },
     extend: {},
   },
   plugins: [],
