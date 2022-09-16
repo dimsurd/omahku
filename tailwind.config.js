cbd8c8012a89538106cadca0afecf07f88292c62
Dimas Surya Dilana <dimassuryadilana@gmail.com>
2022-09-16 09:45:55 +0700

    add beberapa section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
   theme: {
     colors: {
       ungu: "#4A6FFF",
+      unguTua: "#2451FF",
       white: "#ffffff",
       abu: "#95A2A9",
       abuTua: "#9F9F9F",
       abuMuda: "#D3D3D3",
       black: "#2F2F2F",
+      black2: "#212121",
+      black3: "#373737",
     },
     extend: {},
   },
