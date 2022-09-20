e44f5082984a1417dbe9528d896064c64a874870
Dimas Surya Dilana <dimassuryadilana@gmail.com>
2022-09-20 16:50:44 +0700

    add swiper js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ module.exports = {
       abu: "#95A2A9",
       abuTua: "#9F9F9F",
       abuMuda: "#D3D3D3",
+      abuBorder: "#E9EEF3",
       black: "#2F2F2F",
       black2: "#212121",
       black3: "#373737",
       orange: "#fa5a09",
+      beureum: "#FF0000",
     },
     extend: {
       fontFamily: {
